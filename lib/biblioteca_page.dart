55c8f9d1e756b65a30fbd03d12dabd3a099281aa
Tiago212112 <tiagoabreu072@gmail.com>
2026-05-25 10:29:16 -0300

    Update biblioteca_page.dart

--- lib/biblioteca_page.dart
+++ lib/biblioteca_page.dart
@@ -9,60 +9,138 @@ class BibliotecaPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor:  const Color.fromARGB(255, 4, 23, 56),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Icon(Icons.search),
+            Icon(Icons.search, color: Colors.white,),
           ],
         ),
       ),
       backgroundColor: const Color.fromARGB(255, 4, 23, 56),
 
-      body: Column(
-        children: [
-          Text("Sua biblioteca"),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("assets/images/alan.png", width: 186,),
-              Image.asset("assets/images/alan.png", width: 186,),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("assets/images/alan.png", width: 186,),
-              Image.asset("assets/images/alan.png", width: 186,),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset("assets/images/alan.png", width: 186,),
-              Image.asset("assets/images/alan.png", width: 186,),
-            ],
-          ),
-
-
-          ElevatedButton(onPressed: (){}, 
-          style: ElevatedButton.styleFrom(
-            minimumSize: Size(900, 50),
-            maximumSize: Size(900, 50),
-            backgroundColor: Colors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadiusGeometry.circular(10),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text("Sua biblioteca"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white, 
+                      width: 3,
+                      ),
+                  ),
+                  child: Image.asset("assets/images/alan.png", width: 180,)),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white, 
+                      width: 3,
+                      ),
+                  ),
+                  child: Image.asset("assets/images/alan.png", width: 180,)),
+                
+              ],
             ),
-            textStyle: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.bold, 
-              ),   
-          ),
-          child: Text("Adicionar um jogo", )),
-
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white, 
+                      width: 3,
+                      ),
+                  ),
+                  child: Image.asset("assets/images/alan.png", width: 180,)),
+                  Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white, 
+                      width: 3,
+                      ),
+                  ),
+                  child: Image.asset("assets/images/alan.png", width: 180,)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white, 
+                      width: 3,
+                      ),
+                  ),
+                  child: Image.asset("assets/images/alan.png", width: 180,)),
+                  Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white, 
+                      width: 3,
+                      ),
+                  ),
+                  child: Image.asset("assets/images/alan.png", width: 180,)),
+              ],
+            ),
+        
+        
+            ElevatedButton(onPressed: (){}, 
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size(900, 50),
+              maximumSize: Size(900, 50),
+              backgroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              textStyle: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold, 
+                ),   
+            ),
+            child: Text("Adicionar um jogo", )),
+        
+            SizedBox(height: 20,
+            ),
+            
+            Container(
+              width: 360,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+            SizedBox(height: 20,
+            ),
+            
+            Container(
+              width: 360,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+            SizedBox(height: 20,
+            ),
+            
+            Container(
+              width: 360,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            )
+          ],
           
-        ],
-        
-        
+          
+        ),
       ),
          
     bottomNavigationBar: BottomAppBar(
