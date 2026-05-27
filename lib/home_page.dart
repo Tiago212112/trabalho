5020a1d59c367d676e9442267f71b4467736efa1
Tiago212112 <tiagoabreu072@gmail.com>
2026-05-27 10:24:27 -0300

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trabalho/screens/biblioteca_page.dart';
+import 'package:trabalho/screens/chat_page.dart';
 import 'package:trabalho/screens/perfil_page.dart';
 
 class HomePage extends StatelessWidget {
@@ -24,33 +25,44 @@ class HomePage extends StatelessWidget {
           ),
           Align(
             alignment: Alignment.centerRight,
-            child: Container(
-              height: 200,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 5,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 50),
+                  child: Container(
+                     decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 3,
+                      )
+                     ),
+                    child: Expanded(
+                      child: Image.network("https://upload.wikimedia.org/wikipedia/pt/thumb/d/d3/The_Last_of_Us_Part_I.png/250px-The_Last_of_Us_Part_I.png", width: 100,)),
                   ),
-                color: Colors.white
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Image.asset("assets/images/re9.jpg",)),
-                  Expanded(
-                    child: Image.asset("assets/images/tekken.jpg", 
-                    
-                  ))
-                ],
-              ),
+                ),
+
+                  SizedBox(width: 50,),
+
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 3,
+                    )
+                  ),
+                  child: Expanded(
+                    child: Image.network("https://store-images.s-microsoft.com/image/apps.43812.14322949163163356.dae4c2ae-9fa8-482a-9815-7473f8376992.169cc773-a4fd-4726-a9c1-7674c007c758", 
+                    width: 100,
+                  )),
+                )
+              ],
             ),
           ),
                  ListTile(
     leading: CircleAvatar(
       backgroundColor: Colors.white,
       radius: 30,
-      child: Image.asset("assets/images/user.png",
+      child: Image.network("https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
        fit: BoxFit.cover,
        width: 30,
        height: 30,
@@ -65,14 +77,16 @@ class HomePage extends StatelessWidget {
         style: TextStyle(color: Colors.greenAccent),
         ),
         trailing: IconButton(
-          onPressed: (){}, 
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage()));
+          }, 
           icon: Icon(Icons.chat, color: Colors.white,)),
   ), 
                  ListTile(
     leading: CircleAvatar(
       backgroundColor: Colors.white,
       radius: 30,
-      child: Image.asset("assets/images/user.png",
+      child: Image.network("https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
        fit: BoxFit.cover,
        width: 30,
        height: 30,
@@ -87,14 +101,16 @@ class HomePage extends StatelessWidget {
         style: TextStyle(color: Colors.greenAccent),
         ),
         trailing: IconButton(
-          onPressed: (){}, 
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage()));
+          }, 
           icon: Icon(Icons.chat, color: Colors.white,)),
   ),
                  ListTile(
     leading: CircleAvatar(
       backgroundColor: Colors.white,
       radius: 30,
-      child: Image.asset("assets/images/user.png",
+      child: Image.network("https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
        fit: BoxFit.cover,
        width: 30,
        height: 30,
@@ -109,14 +125,16 @@ class HomePage extends StatelessWidget {
         style: TextStyle(color: Colors.greenAccent),
         ),
         trailing: IconButton(
-          onPressed: (){}, 
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage()));
+          }, 
           icon: Icon(Icons.chat, color: Colors.white,)),
   ),
                  ListTile(
     leading: CircleAvatar(
       backgroundColor: Colors.white,
       radius: 30,
-      child: Image.asset("assets/images/user.png",
+      child: Image.network("https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png",
        fit: BoxFit.cover,
        width: 30,
        height: 30,
@@ -131,7 +149,9 @@ class HomePage extends StatelessWidget {
         style: TextStyle(color: Colors.redAccent),
         ),
         trailing: IconButton(
-          onPressed: (){}, 
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage()));
+          }, 
           icon: Icon(Icons.chat, color: Colors.white,)),
   ),
          ],
