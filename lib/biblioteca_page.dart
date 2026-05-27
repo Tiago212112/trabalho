c888d066ae9220a7fce4912abd45b7cba8e69592
Tiago212112 <tiagoabreu072@gmail.com>
2026-05-27 09:47:59 -0300

    Update biblioteca_page.dart

--- lib/biblioteca_page.dart
+++ lib/biblioteca_page.dart
@@ -33,7 +33,7 @@ class BibliotecaPage extends StatelessWidget {
                       width: 3,
                       ),
                   ),
-                  child: Image.asset("assets/images/alan.png", width: 180,)),
+                  child: Image.network("https://store-images.s-microsoft.com/image/apps.7759.14335040691238971.69596c0b-00e0-49eb-a6ce-2535ef602b5e.d8780f81-4399-46f1-bd6d-4027f002304d?q=90&w=480&h=270", width: 180,)),
                 Container(
                   decoration: BoxDecoration(
                     border: Border.all(
@@ -41,7 +41,7 @@ class BibliotecaPage extends StatelessWidget {
                       width: 3,
                       ),
                   ),
-                  child: Image.asset("assets/images/alan.png", width: 180,)),
+                  child: Image.network("https://store-images.s-microsoft.com/image/apps.44561.67854144654891351.62e8ac86-d08d-4b3e-a943-500d5e0b2692.7c8422ad-1ebc-47b3-950c-a8fa0e652860?q=90&w=480&h=270", width: 180,)),
                 
               ],
             ),
@@ -55,7 +55,7 @@ class BibliotecaPage extends StatelessWidget {
                       width: 3,
                       ),
                   ),
-                  child: Image.asset("assets/images/alan.png", width: 180,)),
+                  child: Image.network("https://store-images.s-microsoft.com/image/apps.63980.14322949163163356.b2901018-eef2-46a0-8e0d-103bc209b3b9.b6dc9f72-2764-47ee-bbf6-c3474f7eeb67?q=90&w=480&h=270", width: 180,)),
                   Container(
                   decoration: BoxDecoration(
                     border: Border.all(
@@ -63,7 +63,7 @@ class BibliotecaPage extends StatelessWidget {
                       width: 3,
                       ),
                   ),
-                  child: Image.asset("assets/images/alan.png", width: 180,)),
+                  child: Image.network("https://cdn2.unrealengine.com/egs-thelastofusparti-naughtydogllc-s1-2560x1440-586e8a00974c.jpg?resize=1&w=480&h=270&quality=medium", width: 180,)),
               ],
             ),
             Row(
@@ -76,7 +76,7 @@ class BibliotecaPage extends StatelessWidget {
                       width: 3,
                       ),
                   ),
-                  child: Image.asset("assets/images/alan.png", width: 180,)),
+                  child: Image.network("https://cdn1.epicgames.com/offer/b2818b59c0bb420e9647983dfd254931/EGS_Octopus_InsomniacGamesNixxesSoftware_S1_2560x1440-f27da78f484626718d1e22e7d6950ca5?resize=1&w=480&h=270&quality=medium", width: 180,)),
                   Container(
                   decoration: BoxDecoration(
                     border: Border.all(
@@ -84,7 +84,7 @@ class BibliotecaPage extends StatelessWidget {
                       width: 3,
                       ),
                   ),
-                  child: Image.asset("assets/images/alan.png", width: 180,)),
+                  child: Image.network("https://store-images.s-microsoft.com/image/apps.32258.13847644057609868.8865d2a2-5e11-4424-85ce-1db30a161bd9.e67ef3ac-9287-4a7d-a6aa-58c84193c98d?q=90&w=480&h=270", width: 180,)),
               ],
             ),
         
